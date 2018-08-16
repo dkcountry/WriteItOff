344d8c177663e4acdf628867b32da0c50d390fc5
David Kang <dkang009@gmail.com>
2018-08-15 21:35:36 -0500

    init commit

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const Index = () => {
+  return <div>Welcome to Write it Off</div>;
+};
+
+ReactDOM.render(<Index />, document.getElementById("index"));
---
+++ webpack.config.js
@@ -0,0 +1,46 @@
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+
+const htmlWebpackPlugin = new HtmlWebPackPlugin({
+  template: "./src/index.html",
+  filename: "./index.html"
+});
+
+module.exports = {
+    devServer: { 
+        inline: true, 
+        contentBase: './dist', 
+        port: 3001, 
+        proxy: { "/**": { target: 'http://localhost:3001', secure: false }  },
+        historyApiFallback: true
+     },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      },
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: "style-loader"
+          },
+          {
+            loader: "css-loader",
+            options: {
+              modules: true,
+              importLoaders: 1,
+              localIdentName: "[name]_[local]_[hash:base64]",
+              sourceMap: true,
+              minimize: true
+            }
+          }
+        ]
+      }
+    ]
+  },
+  plugins: [htmlWebpackPlugin]
+};
