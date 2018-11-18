3861683c662a5a3a37f0a979e112c975a28ea2f5
David Kang <dkang009@gmail.com>
2018-11-18 14:47:44 -0800

    gif added

--- webpack.config.js
+++ webpack.config.js
@@ -30,7 +30,7 @@ module.exports = {
             }
         },
         {
-            test: /\.(jpg|png)$/,
+            test: /\.(jpg|png|gif)$/,
             use: {
               loader: "url-loader",
               options: {
