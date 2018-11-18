8d98ae16045cede2985a590a781f5d85d3d75676
Paul Koullick <pkoullick@gmail.com>
2018-11-18 22:30:31 +0000

    making vid great again

--- src/components/mp4loader.js
+++ src/components/mp4loader.js
@@ -3,19 +3,26 @@ import mp4file1 from '../../public/keeper-1.mp4';
 import mp4file2 from '../../public/keeper-2.mp4';
 import mp4file3 from '../../public/keeper-3.mp4';
 import * as styles from "../styles";
+import MediaQuery from 'react-responsive';
+import gif from '../../public/keeper.gif';
 
 
 class MP4loader extends React.Component {
     render() {
       return (
         <div>
-            <video webkit-playsinline="true" autoPlay muted src={mp4file1} type='video/mp4;codecs="avc1.42E01E, mp4a.40.2"' style={styles.MP4video} /> 
-            <video webkit-playsinline="true" autoPlay muted style={styles.MP4video}> 
-              <source src={mp4file2} type='video/mp4;codecs="avc1.42E01E, mp4a.40.2"'/>
-            </video>
-            <video webkit-playsinline="true" autoPlay muted style={styles.MP4video}> 
-              <source src={mp4file3} type='video/mp4;codecs="avc1.42E01E, mp4a.40.2"'/>
-            </video>
+            <MediaQuery query='(max-width: 1000px)'>
+              <img className="logo" src={gif} style={styles.imageWidth}/>
+            </MediaQuery>
+            <MediaQuery query='(min-width: 1001px)'>
+              <video webkit-playsinline="true" loop autoPlay muted src={mp4file1} type='video/mp4;codecs="avc1.42E01E, mp4a.40.2"' style={styles.MP4video} /> 
+              <video webkit-playsinline="true" loop autoPlay muted style={styles.MP4video}> 
+                <source src={mp4file2} type='video/mp4;codecs="avc1.42E01E, mp4a.40.2"'/>
+              </video>
+              <video webkit-playsinline="true" loop autoPlay muted style={styles.MP4video}> 
+                <source src={mp4file3} type='video/mp4;codecs="avc1.42E01E, mp4a.40.2"'/>
+              </video>
+            </MediaQuery>
         </div>
       );
     }
